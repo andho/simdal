350032f011af51a476382f780e876cb7444690b8
Amjad Mohamed <amjad@amjad.alliedinsure.com.mv>
2011-02-19 11:44:24 +0500

    Test include changed to be more portable

--- tests/PHPUnit/SimDAL_Unit_TestCase.php
+++ tests/PHPUnit/SimDAL_Unit_TestCase.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../TestDomain/init.php';
+require_once realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '../TestDomain/init.php';
 
 class SimDAL_Unit_TestCase extends PHPUnit_Framework_TestCase {
 	
