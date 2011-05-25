a9e441e085693738bf32d4a489433ab5d7d632a3
Andho <master@andho.com>
2011-05-25 15:36:31 +0500

    Creating PDO adapter and refactoring queryToString into a separate class

--- Persistence/DBAdapterAbstract.php
+++ Persistence/DBAdapterAbstract.php
@@ -2,6 +2,10 @@
 
 abstract class SimDAL_Persistence_DBAdapterAbstract extends SimDAL_Persistence_AdapterAbstract {
 	
+	public function __construct(SimDAL_Mapper $mapper=null, SimDAL_Session $session=null, $conf) {
+		$this->_conn = $connection;
+	}
+	
 	public function __destruct() {
 		$this->_disconnect();
 	}
---
+++ Persistence/PDO/PDOAbstract.php
@@ -0,0 +1,12 @@
+<?php
+
+class SimDAL_Persistence_PDO_PDOAbstract extends SimDAL_Persistence_DBAdapterAbstract {
+	
+	/**
+	 * 
+	 * PDO object
+	 * @var PDO
+	 */
+	private $_conn;
+	
+}
